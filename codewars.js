d901f28396836d59c7cbbfaec5e5af3c744a0d35
Young Lee <ylee8575@gmail.com>
2017-11-28 16:57:05 -0800

    November 28, 2017 exercises

--- codewars.js
+++ codewars.js
@@ -72,6 +72,65 @@ function getCount(str) {
   return str.replace(/[^aeiou]/gi, '').length;
 }
 
+/* Get the Middle Character: You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+#Examples:
+
+Kata.getMiddle("test") should return "es"
+
+Kata.getMiddle("testing") should return "t"
+
+Kata.getMiddle("middle") should return "dd"
+
+Kata.getMiddle("A") should return "A"
+#Input
+
+A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+
+#Output
+
+The middle character(s) of the word represented as a string. */
+
+function getMiddle(s) {
+  if (s.length === 1) {
+    return s;
+  }
+  var mid = (s.length / 2);
+  if (Number.isInteger(mid)) {
+    return s.slice((mid - 1), mid + 1);
+  } else if (!Number.isInteger(mid)) {
+    var jay = Math.floor(mid);
+    return s.slice(jay, (jay + 1));
+  }
+
+}
+
+function getMiddle(s) {
+  return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+}
+
+/* Disemvowel Trolls: Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel. */
+
+function disemvowel(str) {
+  var vowels = 'aeiou'.split('');
+
+  return str.split('').filter(function(x) {
+    return vowels.indexOf(x.toLowerCase()) === -1;
+  }).join('');
+}
+
+function disemvowel(str) {
+  return str.replace(/[aeiou]/gi, '');
+}
+
 // 8 kyu problems //
 
 /* Even or Odd - Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. */
@@ -96,4 +155,99 @@ function opposite(number) {
 
 function opposite(number) {
   return(-number);
+}
+
+/* Jenny's Secret Message: Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+
+Can you help her?
+
+Test.describe("Jenny's greeting function", function(){
+  Test.it("should greet some people normally",function(){
+    Test.assertEquals(greet("Jim"), "Hello, Jim!");
+    Test.assertEquals(greet("Jane"), "Hello, Jane!");
+    Test.assertEquals(greet("Simon"), "Hello, Simon!");
+  });
+  Test.it("should greet Johnny a little bit more special",function(){
+    Test.assertEquals(greet("Johnny"), "Hello, my love!");
+  });
+}); */
+
+function greet(name){
+  if(name === "Johnny")
+    return "Hello, my love!";
+  else
+    return "Hello, " + name + "!";
+}
+
+function greet(name){
+  return "Hello, " + (name == "Johnny" ? "my love" : name) + "!";
+}
+
+/* Convert a Number to a String: We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+
+numberToString(123); // returns '123';`
+numberToString(999); // returns '999';` */
+
+function numberToString(num) {
+  var n=num.toString();
+  return n;
+}
+
+function numberToString(num) {
+  return num.toString();
+}
+
+/* Counting Sheep: Consider an array of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true] */
+
+function countSheeps(arrayOfSheep) {
+  var count = 0;
+  for (var i = 0; i < arrayOfSheep.length; i++) {
+    var sheep = arrayOfSheep[i];
+    if (sheep === true) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+function countSheeps(arrayOfSheeps) {
+  return arrayOfSheeps.filter(Boolean).length;
+}
+
+/* Return Negative: In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+Example:
+
+makeNegative(1); // return -1
+makeNegative(-5); // return -5
+makeNegative(0); // return 0
+Notes:
+
+The number can be negative already, in which case no change is required.
+Zero (0) can't be negative, see examples above. */
+
+function makeNegative(num) {
+  if (num === 0 || num < 0) {
+    return num;
+  };
+  if (num > 0) {
+    return num * -1;
+  };
+}
+
+function makeNegative(num) {
+  return -Math.abs(num);
 }
--- leetcode.js
+++ leetcode.js
@@ -13,6 +13,52 @@ var reverseString = function(s) {
     return reversed.join('')
 };
 
+/* Fizz Buzz: Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+
+Example:
+
+n = 15,
+
+Return:
+[
+    "1",
+    "2",
+    "Fizz",
+    "4",
+    "Buzz",
+    "Fizz",
+    "7",
+    "8",
+    "Fizz",
+    "Buzz",
+    "11",
+    "Fizz",
+    "13",
+    "14",
+    "FizzBuzz"
+] */
+
+var fizzBuzz = function(n) {
+  const finalArr = []
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      finalArr.push('FizzBuzz')
+    }
+    else if (i % 3 === 0) {
+      finalArr.push('Fizz')
+    }
+    else if (i % 5 === 0) {
+      finalArr.push('Buzz')
+    }
+    else {
+      finalArr.push(i.toString())
+    }
+  }
+  return finalArr
+};
+
 // Medium Difficulty //
 
 // Hard Difficulty //
