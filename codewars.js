0520617dddbc1bfa37d65843a6f9803c9c90857b
Young Lee <ylee8575@gmail.com>
2017-12-13 23:16:08 -0800

    December 13, 2017 exercises

--- codewars.js
+++ codewars.js
@@ -281,6 +281,37 @@ function findShort(s){
   return Math.min(...s.split(" ").map (s => s.length));
 }
 
+/* Credit Card Mask: Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+Examples
+
+maskify("4556364607935616") == "############5616"
+maskify(     "64607935616") ==      "#######5616"
+maskify(               "1") ==                "1"
+maskify(                "") ==                 ""
+
+// "What was the name of your first pet?"
+maskify("Skippy")                                   == "##ippy"
+maskify("Nananananananananananananananana Batman!") == "####################################man!" */
+
+function maskify(cc) {
+  var newArr = [];
+  var chars = cc.split("");
+  for (var i = 0; i < chars.length - 4; i++) {
+    newArr.push("#");
+  };
+  var lastFour = cc.slice(-4);
+  var hashtags = newArr.join("");
+  var final = hashtags + lastFour;
+  return final;
+}
+
+function maskify(cc) {
+  return cc.slice(0, -4).replace(/./g, '#') + cc.slice(-4);
+}
+
 // 8 kyu problems //
 
 /* Even or Odd - Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. */
