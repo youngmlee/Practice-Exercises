489bc0f02055255cccccb9cb4b0a1db267debf76
Young Lee <ylee8575@gmail.com>
2017-12-15 22:56:51 -0800

    December 15, 2017 exercises

--- codewars.js
+++ codewars.js
@@ -988,3 +988,27 @@ function greet (name, owner) {
 function greet (name, owner) {
   return name === owner ? 'Hello boss' :   'Hello guest';
 }
+
+/* Exclamation marks series #4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string: Description:
+
+Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+
+Examples
+
+remove("Hi!") === "Hi!"
+remove("Hi!!!") === "Hi!"
+remove("!Hi") === "Hi!"
+remove("!Hi!") === "Hi!"
+remove("Hi! Hi!") === "Hi Hi!"
+remove("Hi") === "Hi!"*/
+
+function remove(s){
+  var split = s.split('!');
+  var joined = split.join('')
+  var final = joined + '!';
+  return final;
+}
+
+function remove(s) {
+  return s.replace(/!+/g, '') + '!';
+}
