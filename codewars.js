930b49bb281774295ce96a31c18e694748f28743
Young Lee <ylee8575@gmail.com>
2017-12-20 16:35:33 -0800

    December 20, 2017 exercises

--- codewars.js
+++ codewars.js
@@ -348,6 +348,25 @@ function maskify(cc) {
   return cc.slice(0, -4).replace(/./g, '#') + cc.slice(-4);
 }
 
+/* The Coupon Code: Your online store likes to give out coupons for special occasions. Some customers try to cheat the system by entering invalid codes or using expired coupons.
+
+Your mission:
+Write a function called checkCoupon to verify that a coupon is valid and not expired. If the coupon is good, return true. Otherwise, return false.
+
+A coupon expires at the END of the expiration date. All dates will be passed in as strings in this format: "June 15, 2014" */
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+  if (enteredCode === correctCode && Date.parse(currentDate) <= Date.parse(expirationDate)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+  return enteredCode === correctCode && Date.parse(expirationDate) >= Date.parse(currentDate)
+}
+
 // 8 kyu problems //
 
 /* Even or Odd - Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. */
