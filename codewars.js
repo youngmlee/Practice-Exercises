5cc0665f0cd09c54a8deb4637ab618a2d8d1ec56
Young Lee <ylee8575@gmail.com>
2017-12-21 22:54:03 -0800

    December 21, 2017 exercises

--- codewars.js
+++ codewars.js
@@ -367,6 +367,32 @@ function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
   return enteredCode === correctCode && Date.parse(expirationDate) >= Date.parse(currentDate)
 }
 
+/* Folding Your Way to the Moon: Have you heard about the myth that if you fold a paper enough times, you can reach the moon with it? Sure you do, but exactly how many? Maybe it's time to write a program to figure it out.
+
+You know that a piece of paper has a thickness of 0.0001m. Given distance in units of meters, calculate how many times you have to fold the paper to make the paper reach this distance.
+(If you're not familiar with the concept of folding a paper: Each fold doubles its total thickness.)
+
+Note: Of course you can't do half a fold. You should know what this means ;P
+
+Also, if somebody is giving you a non-positive distance, it's clearly bogus and you should yell at them by returning null (or whatever equivalent in your language). */
+
+function foldTo(distance) {
+  var folds = 0;
+  var dist = 0.0001;
+  if (distance <= 0) {
+    folds = null;
+  }
+  while (distance > dist) {
+    dist = dist * 2;
+    folds += 1;
+  }
+  return folds;
+}
+
+function foldTo(n) {
+  return n > 0 ? Math.max(0, Math.ceil(Math.log2(1e4 * n))) : null;
+}
+
 // 8 kyu problems //
 
 /* Even or Odd - Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. */
