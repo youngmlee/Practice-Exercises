37031c1a1aff2aa87fc6168d386f9d9a0348a543
Young Lee <ylee8575@gmail.com>
2018-01-09 14:56:40 -0800

    January 9, 2018 exercises

--- codewars.js
+++ codewars.js
@@ -634,6 +634,26 @@ function moveVowel(input) {
   return cs + vs;
 }
 
+/* Sum of all the multiples of 3 or 5: Your task is to write function findSum.
+
+Upto and including n, this function will return the sum of all multiples of 3 and 5.
+
+For example:
+
+findSum(5) should return 8 (3 + 5)
+
+findSum(10) should return 33 (3 + 5 + 6 + 9 + 10) */
+
+function findSum(n) {
+  var sum = 0;
+  for (var i = 0; i <= n; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+    }
+  }
+  return sum;
+}
+
 // 8 kyu problems //
 
 /* Even or Odd - Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. */
