308090c2071744770fb0828334b336ca3e87b539
Young Lee <ylee8575@gmail.com>
2018-01-15 23:54:38 -0800

    January 15, 2018 exercises

--- codewars.js
+++ codewars.js
@@ -1462,3 +1462,28 @@ function removeChar(str) {
 function makeUpperCase(str) {return str.toUpperCase();}
 
 const makeUpperCase = str => str.toUpperCase();
+
+/* Array plus array: I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too. */
+
+function arrayPlusArray(arr1, arr2) {
+  let sum = 0;
+  if (arr1.length >= arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      sum += +arr1[i];
+      sum += +arr2[i];
+    }
+    return sum;
+  } else {
+    for (let i = 0; i < arr2.length; i++) {
+      sum += +arr1[i];
+      sum += +arr2[i];
+    }
+    return sum;
+  }
+}
+
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+}
