f20c4f70344d099f781049469680050083f16883
Young Lee <ylee8575@gmail.com>
2018-01-17 23:14:17 -0800

    January 17, 2018 exercises

--- codewars.js
+++ codewars.js
@@ -1503,3 +1503,18 @@ function oddCount(n){
   }
   return oddCount;
 }
+
+/* USD => CNY: Create a function that converts US dollars (USD) to Chinese Yuan (CNY) . The input is the amount of USD as an integer, and the output should be a string that states the amount of Yuan followed by 'Chinese Yuan'
+
+For Example:
+
+  usdcny(15) // => '101.25 Chinese Yuan'
+  usdcny(465) // => '3138.75 Chinese Yuan'
+The conversion rate you should use is 6.75 CNY for every 1 USD. All numbers shold be reounded to the nearest hundreth (e.g. 21.00 NOT 21.0 or 21) (HINT: google toFixed) */
+
+function usdcny(usd) {
+  let yuan = (usd * 6.75).toFixed(2)
+  return yuan.toString() + ' Chinese Yuan'
+}
+
+usdcny = $ => `${($ * 6.75).toFixed(2)} Chinese Yuan`;
