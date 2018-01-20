f8eead5ade2a2c4f2ef488e1a9f547dcf588c8a6
Young Lee <ylee8575@gmail.com>
2018-01-19 22:10:05 -0800

    January 19, 2018 exercises

--- codewars.js
+++ codewars.js
@@ -1530,3 +1530,21 @@ function hero(bullets, dragons){
 function hero(bullets, dragons){
   return bullets >= dragons * 2
 }
+
+/* Man in the West: A western man is tring to find gold in a river. To do that, he passes a bucket through the river's soil and then checks if it contains any gold. However, he could be more productive if he wrote an algorithm to do the job for him.
+
+So, you need to check if there is gold in the bucket, and if so, return True/true. If not, return False/false.*/
+
+function checkTheBucket(bucket){
+  let def = false;
+  for (let i = 0; i < bucket.length; i++) {
+    if (bucket[i] === "gold") {
+      def = true;
+    }
+  }
+  return def
+}
+
+function checkTheBucket(bucket){
+  return bucket.includes('gold');
+}
