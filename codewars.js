b126864e6d5879cc589e4e85cead306ebaa35c54
Young Lee <ylee8575@gmail.com>
2018-01-26 14:22:46 -0800

    January 26, 2018 exercises

--- codewars.js
+++ codewars.js
@@ -1628,3 +1628,24 @@ function getAverage(marks){
 function getAverage(marks){
   return Math.floor(marks.reduce((sum, x) => sum + x) / marks.length);
 }
+
+/* Add Length: What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
+
+addLength('apple ban') => ["apple 5", "ban 3"]
+addLength('you will win') => ["you 3", "will 4", "win 3"]
+Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
+
+Note: String will have at least one element; words will always be separated by a space. */
+
+function addLength(str) {
+  let finalArr = []
+  const wordsArr = str.split(' ')
+  for (let i = 0; i < wordsArr.length; i++) {
+    finalArr.push(wordsArr[i] + ' ' + wordsArr[i].length);
+  }
+  return finalArr;
+}
+
+function addLength(str){
+  return str.split(' ').map(function(v){return v+' '+v.length})
+}
