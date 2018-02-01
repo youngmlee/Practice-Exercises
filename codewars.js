1434496d0e15511bc55adf292f9a4dde748d4eed
Young Lee <ylee8575@gmail.com>
2018-01-31 22:43:55 -0800

    January 31, 2018 exercises

--- codewars.js
+++ codewars.js
@@ -1698,3 +1698,26 @@ function digitize(n) {
 function digitize(n) {
   return String(n).split('').map(Number);
 }
+
+/* Find Maximum and Minimum Values of a List: Your task is to make two functions, max and min (maximum and minimum in PHP) that take a(n) array/vector of integers list as input and outputs, respectively, the largest and lowest number in that array/vector.
+
+#Examples
+
+max([4,6,2,1,9,63,-134,566]) returns 566
+min([-52, 56, 30, 29, -54, 0, -110]) returns -110
+max([5]) returns 5
+min([42, 54, 65, 87, 0]) returns 0
+#Notes
+
+You may consider that there will not be any empty arrays/vectors. */
+
+var min = function(list){
+  return Math.min(...list)
+}
+
+var max = function(list){
+  return Math.max(...list)
+}
+
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
