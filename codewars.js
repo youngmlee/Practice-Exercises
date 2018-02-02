57bfee5fb298c6415d4b9914994a48fac6fdac52
YoungNotTheOld1 <ylee8575@gmail.com>
2018-02-01 20:57:31 -0800

    February 1, 2018 exercises

--- codewars.js
+++ codewars.js
@@ -700,6 +700,42 @@ function magicSum(a) {
   return Array.isArray(a) ? a.reduce((a,b)=>a+((''+b).includes('3') && b%2 ? b : 0),0) : 0
 }
 
+/* Categorize New Member: The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+Input
+Input will consist of a list of lists containing two items each. Each list contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+Note for F#: The input will be of (int list list) which is a List>
+
+Example Input
+[[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]]
+Output
+Output will consist of a list of string values (in Haskell: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+
+Example Output
+["Open", "Open", "Senior", "Open", "Open", "Senior"] */
+
+function openOrSenior(data){
+  const output = []
+  for (let i = 0; i < data.length; i++) {
+    if ((data[i][0] >= 55) && (data[i][1] > 7)) {
+      output.push("Senior")
+    } else {
+       output.push("Open")
+     }
+  }
+  return output
+}
+
+function openOrSenior(data){
+  function determineMembership(member){
+    return (member[0] >= 55 && member[1] > 7) ? 'Senior' : 'Open';
+  }
+  return data.map(determineMembership);
+}
+
 // 8 kyu problems //
 
 /* Even or Odd - Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. */
