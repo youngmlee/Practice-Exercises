db9d9fa17120457621573c6830e6ea33716420a8
Young Lee <ylee8575@gmail.com>
2018-02-07 13:31:43 -0800

    February 7, 2018 exercises

--- codewars.js
+++ codewars.js
@@ -785,6 +785,30 @@ function minMax(arr){
   return [Math.min(...arr), Math.max(...arr)];
 }
 
+/* Highest and lowest: In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+Example:
+
+highAndLow("1 2 3 4 5"); // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+Notes:
+
+All numbers are valid Int32, no need to validate them.
+There will always be at least one number in the input string.
+Output string must be two numbers separated by a single space, and highest number is first. */
+
+function highAndLow(numbers){
+  const strArr = numbers.split(' ')
+  const numArr = strArr.map( x => +x)
+  return (Math.max(...numArr) + ' ' + Math.min(...numArr))
+}
+
+function highAndLow(numbers){
+  numbers = numbers.split(' ').map(Number);
+  return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
+}
+
 // 8 kyu problems //
 
 /* Even or Odd - Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. */
