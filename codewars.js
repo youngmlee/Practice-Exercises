80be3586e77c1e608c241fdb2d6ce1afbb519e71
Young Lee <ylee8575@gmail.com>
2019-08-26 10:47:57 -0700

    8/26/19 Codewars

--- codewars.js
+++ codewars.js
@@ -163,6 +163,14 @@ function reverseWords(str){
 
 // 7 kyu problems //
 
+/* Descending Order
+
+Your task is to make a function that can take any non-negative integer as a argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.*/
+
+function descendingOrder(n){
+  return parseInt(String(n).split('').sort().reverse().join(''))
+}
+
 /* Vowel Count: Return the number (count) of vowels in the given string.
 
 We will consider a, e, i, o, and u as vowels for this Kata.
