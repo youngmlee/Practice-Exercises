70c4ff47be69aeac8085afffd627774316aa6d59
Young Lee <ylee8575@gmail.com>
2019-08-27 11:41:42 -0700

    8/27/19 Coderbyte

--- coderbyte.js
+++ coderbyte.js
@@ -125,3 +125,69 @@ function LetterChanges(str) {
 // Medium Difficulty //
 
 // Hard Difficulty //
+
+/* Challenge
+Have the function MaximalSquare(strArr) take the strArr parameter being passed which will be a 2D matrix of 0 and 1's, and determine the area of the largest square submatrix that contains all 1's. A square submatrix is one of equal width and height, and your program should return the area of the largest submatrix that contains only 1's. For example: if strArr is ["10100", "10111", "11111", "10010"] then this looks like the following matrix: 
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0 
+
+For the input above, you can see the bolded 1's create the largest square submatrix of size 2x2, so your program should return the area which is 4. You can assume the input will not be empty. 
+
+Hard challenges are worth 15 points and you are not timed for them.
+Sample Test Cases
+Input:["0111", "1111", "1111", "1111"]
+
+Output:9
+
+
+Input:["0111", "1101", "0111"]
+
+Output:1 */
+
+function MaximalSquare(strArr) {
+  let fullArray = strArr.map(val => val.split(''));
+
+  let initArray = initialEval(fullArray);
+
+  if (initArray.length === 0) {
+    return 0;
+  }
+  count = 1;
+
+  while (initArray.length) {
+    initArray = initArray.filter(val => {
+      return testOne(fullArray, count + 1, val[0], val[1]);
+    })
+    count++;
+    if (count > 10) break;
+  }
+  return Math.pow(count - 1, 2);
+}
+
+function initialEval (arr) {
+  let initArray = [];
+  arr.forEach((row, rowNum) => {
+    row.forEach((val, colNum) => {
+      if (val === '1') {
+        initArray.push([rowNum, colNum]);
+      }
+    });
+  });
+  return initArray;
+}
+
+function testOne(arr, size, r, c) {
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      if (!arr[r + i] || !arr[r + i][c + j] || arr[r + i][c + j] !== '1') {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+  
+MaximalSquare(readline());
