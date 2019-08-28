97f5a834d29af2c570bace2981e3bc703fdc755d
Young Lee <ylee8575@gmail.com>
2019-08-28 11:33:41 -0700

    8/28/19 codewars

--- codewars.js
+++ codewars.js
@@ -184,6 +184,30 @@ function reverseWords(str){
 
 // 7 kyu problems //
 
+/* Mumbling
+
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.*/
+
+function accum(s) {
+  return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
+}
+
+function accum(str) {
+  var letters = str.split('');
+  var result = [];
+  for (var i = 0; i < letters.length; i++) {
+    result.push(letters[i].toUpperCase() + Array(i + 1).join(letters[i].toLowerCase()));
+  }
+  return result.join('-');
+}
+
 /* Descending Order
 
 Your task is to make a function that can take any non-negative integer as a argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.*/
