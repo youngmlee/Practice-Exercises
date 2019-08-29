f5bd37c959e61588d6695c56f43f7d477d9508c8
Young Lee <ylee8575@gmail.com>
2019-08-29 12:00:46 -0700

    8/29/19 coderbyte

--- coderbyte.js
+++ coderbyte.js
@@ -191,3 +191,19 @@ function testOne(arr, size, r, c) {
 }
   
 MaximalSquare(readline());
+
+/* Pentagonal Number
+
+Have the function PentagonalNumber(num) read num which will be a positive integer and determine how many dots exist in a pentagonal shape around a center dot on the Nth iteration. For example, in the image below you can see that on the first iteration there is only a single dot, on the second iteration there are 6 dots, on the third there are 16 dots, and on the fourth there are 31 dots. 
+
+ 
+
+Your program should return the number of dots that exist in the whole pentagon on the Nth iteration. 
+
+Hard challenges are worth 15 points and you are not timed for them. */
+
+function PentagonalNumber(num) {
+  return num === 1 ? 1 : PentagonalNumber(num - 1) + (5 * (num - 1));
+}
+   
+PentagonalNumber(readline());
