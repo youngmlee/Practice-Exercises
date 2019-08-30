2f3d22169f2946c1599956848513e70e41ee4926
Young Lee <ylee8575@gmail.com>
2019-08-30 11:20:21 -0700

    8/30/19 codewars

--- codewars.js
+++ codewars.js
@@ -184,6 +184,24 @@ function reverseWords(str){
 
 // 7 kyu problems //
 
+/* Sum of two lowest positive integers
+
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455. */
+
+function sumTwoSmallestNumbers(numbers){  
+  numbers = numbers.sort(function(a, b){return a - b; });
+  return numbers[0] + numbers[1];
+};
+
+function sumTwoSmallestNumbers(numbers) {  
+  var [ a, b ] = numbers.sort((a, b) => a - b)
+  return a + b
+}
+
 /* Mumbling
 
 This time no story, no theory. The examples below show you how to write function accum:
