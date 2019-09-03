3802326324c575c8504a89e2a5a7211f5c54bb90
Young Lee <ylee8575@gmail.com>
2019-09-03 12:27:56 -0700

    9/3/19 leetcode

--- leetcode.js
+++ leetcode.js
@@ -104,6 +104,49 @@ var findTheDifference = function(s, t) {
     }
 };
 
+/* Count and Say
+
+The count-and-say sequence is the sequence of integers with the first five terms as following:
+
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+
+Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence.
+
+Note: Each term of the sequence of integers will be represented as a string.
+
+ 
+
+Example 1:
+
+Input: 1
+Output: "1"
+Example 2:
+
+Input: 4
+Output: "1211" */
+
+var countAndSay = function(n) {
+    if (n == 1) return '1'
+    let prev = countAndSay(n - 1).split('');
+	
+    let result = '';
+    let lastIdx = -1;
+    for (let i = 0; i < prev.length; i++){
+        if (prev[i] !== prev[i+1]) {
+            result += (i - lastIdx) + prev[i];
+            lastIdx = i;
+        }
+    }
+    return result;
+};
+
 /* Reverse Integer: Given a 32-bit signed integer, reverse digits of an integer.
 
 Example 1:
