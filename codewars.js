35d3a9d050940b5f2c77e73575288f68485c3a23
Young Lee <ylee8575@gmail.com>
2019-09-04 22:40:28 -0700

    9/4/19 codewars

--- codewars.js
+++ codewars.js
@@ -184,6 +184,25 @@ function reverseWords(str){
 
 // 7 kyu problems //
 
+/* Two to One
+
+Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters,
+
+each taken only once - coming from s1 or s2.
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz" */
+
+const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('')
+
+function longest(s1, s2) {
+  return Array.from(new Set(s1 + s2)).sort().join('');
+}
+
 /* Sum of two lowest positive integers
 
 Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
