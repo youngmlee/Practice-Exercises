ad5a8adbf9b9d240eb55f4a603b390aed8c55903
Young Lee <ylee8575@gmail.com>
2019-09-09 19:32:37 -0700

    9/9/19 codewars

--- codewars.js
+++ codewars.js
@@ -1211,6 +1211,28 @@ var fizzify = fizzbuzz;
 
 // 8 kyu problems //
 
+/* Century from Year
+
+The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
+
+Task :
+Given a year, return the century it is in.
+
+Input , Output Examples ::
+centuryFromYear(1705)  returns (18)
+centuryFromYear(1900)  returns (19)
+centuryFromYear(1601)  returns (17)
+centuryFromYear(2000)  returns (20)
+Hope you enjoy it .. Awaiting for Best Practice Codes */
+
+function century(year) {
+  return (year + 99) / 100 | 0;
+}
+
+function century(year) {
+  return Math.ceil(year/100); //using ceiling method to round up to nearest century (100)
+}
+
 /* Even or Odd - Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. */
 
 function even_or_odd(number) {
