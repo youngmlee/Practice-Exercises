c164b91bb252db378f84a535ecb4ed3ef0403722
Young Lee <ylee8575@gmail.com>
2019-09-11 22:50:33 -0700

    9/11/19 codewars

--- codewars.js
+++ codewars.js
@@ -184,6 +184,29 @@ function reverseWords(str){
 
 // 7 kyu problems //
 
+/* Sum of odd numbers
+
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the row sums of this triangle from the row index (starting at index 1) e.g.:
+
+rowSumOddNumbers(1); // 1
+rowSumOddNumbers(2); // 3 + 5 = 8 */
+
+function rowSumOddNumbers(n) {
+  return Math.pow(n, 3);
+}
+
+function rowSumOddNumbers(n) {
+  return n*n*n
+}
+
 /* Remove the minimum
 
 The museum of incredible dull things
