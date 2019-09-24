a2021acbe702b2a84e30d4a619e371ae4e2a2d89
Young Lee <ylee8575@gmail.com>
2019-09-23 23:41:35 -0700

    9/23/19 codewars

--- codewars.js
+++ codewars.js
@@ -1405,6 +1405,20 @@ var fizzify = fizzbuzz;
 
 // 8 kyu problems //
 
+/* Lost Without a Map
+
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know. */
+
+function maps(x){
+  return x.map(n => n * 2);
+}
+
 /* Is n divisible by x and y?
 
 Create a function isDivisible(n, x, y) that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
