c2b4ed390de2fc36d77d27c02372ff8fc796f6c4
Young Lee <ylee8575@gmail.com>
2019-09-24 10:52:49 -0700

    9/24/19 codewars

--- codewars.js
+++ codewars.js
@@ -184,6 +184,40 @@ function reverseWords(str){
 
 // 7 kyu problems //
 
+/* Friends
+Friends
+Andrzej was given a task:
+
+There are n jars with pills. In every jar there is a different type of pill and the amount of pills in each jar is infinite. One type of pill makes a person glow about 30 minutes after taking and none of the other types has any effect.
+
+His job is to determine, in which jar are the pills that make a person glow. But there is one catch, he only has 35 minutes to do so.(so he can't take a pill, wait for the results and then take another one, because he wouldn't be able to see the results) Fortunetely, he can take any number of friends he needs with him. On completing the task Andrzej receives one million dollars. You know that Andrzej is very honest, so he will split the money equally with his friends.
+
+Your job is to determine how many friends does Andrzej need to complete the task.(He also wants to make the highest amount of money.)
+
+For example for n = 2
+
+The answer is 0 because he doesn't need any friends, he just needs to take a pill from the first jar and wait for the effects.
+
+For another example for n = 4
+
+The answer is 1 because having pills A B C D Andrzej can take pills A B and the friend can take pills B C
+
+Note: Andrzej is not able to accurately measure time during the task. */
+
+function friends(n){
+
+  if (n>2) {
+    return Math.ceil(Math.log(n)/Math.log(2))-1; 
+  }
+  else {
+    return 0;
+  }
+}
+
+function friends(n){
+  return n<=2 ? 0 : Math.floor(Math.log2(n-1));
+  }
+
 /* Grown of a population
 
 In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
