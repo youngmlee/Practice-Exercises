e9f7bf023ea6ee645cafa44cc6c14bb8be9133d8
Young Lee <ylee8575@gmail.com>
2019-09-25 11:17:52 -0700

    9/25/19 leetcode

--- leetcode.js
+++ leetcode.js
@@ -2,6 +2,51 @@
 
 // Easy Difficulty //
 
+/* Valid Parentheses
+
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+
+Input: "()"
+Output: true
+Example 2:
+
+Input: "()[]{}"
+Output: true
+Example 3:
+
+Input: "(]"
+Output: false
+Example 4:
+
+Input: "([)]"
+Output: false
+Example 5:
+
+Input: "{[]}"
+Output: true */
+
+var isValid = function(s) {
+    if (s === null || s.length <= 0) return true;
+    var cArr = s.split('');
+    var stack = [];
+    for (var c of cArr) {
+        if (c === '[') stack.push(']');
+        else if (c === '{') stack.push('}');
+        else if (c === '(') stack.push(')');
+        else if (stack.length === 0 || c !== stack.pop()) return false;
+    }
+    if (stack.length === 0) return true;
+    return false;
+};
+
 /* Reverse String: Write a function that takes a string as input and returns the string reversed.
 
 Example:
