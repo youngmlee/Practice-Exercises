2e55fdce6ff8fb99d3e5b27407635345163902b2
Young Lee <ylee8575@gmail.com>
2019-10-14 11:10:44 -0700

    10/14/19 leetcode

--- leetcode.js
+++ leetcode.js
@@ -2,6 +2,32 @@
 
 // Easy Difficulty //
 
+/* Maximum Subarray
+
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Follow up:
+
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle. */
+
+var maxSubArray = function(arr) {
+    let maxLocal = (maxGlobal = arr[0]);
+
+	  for (let i = 1; i < arr.length; i += 1) {
+		maxLocal = Math.max(arr[i], maxLocal + arr[i]);
+
+		if (maxLocal > maxGlobal) {
+		  maxGlobal = maxLocal;
+		}
+	  }
+	  return maxGlobal;
+	};
+
 /* Same Tree
 
 Given two binary trees, write a function to check if they are the same or not.
