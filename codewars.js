0d32218f7ce4f36f18b71fc6365835ed97fec484
Young Lee <ylee8575@gmail.com>
2019-10-23 00:31:47 -0700

    10/23/19 codewars

--- codewars.js
+++ codewars.js
@@ -1643,6 +1643,34 @@ var fizzify = fizzbuzz;
 
 // 8 kyu problems //
 
+/* Is the string uppercase?
+
+Is the string uppercase?
+Task
+Add the isUpperCase method to String to see whether the string is ALL CAPS. For example:
+
+"c".isUpperCase() == false
+"C".isUpperCase() == true
+"hello I AM DONALD".isUpperCase() == false
+"HELLO I AM DONALD".isUpperCase() == true
+"ACSKLDFJSgSKLDFJSKLDFJ".isUpperCase() == false
+"ACSKLDFJSGSKLDFJSKLDFJ".isUpperCase() == true
+In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS. */
+
+String.prototype.isUpperCase = function() {
+  return this.toUpperCase() === this.toString();
+}
+
+if (!String.prototype.isUpperCase) {
+  String.prototype.isUpperCase = function () {
+    return this.toUpperCase() === String(this);
+  };
+}
+
+String.prototype.isUpperCase = function () {
+  return !/[a-z]/.test(this);
+};
+
 /* Sum of differences in array
 
 Your task is to sum the differences between consecutive pairs in the array in descending order.
