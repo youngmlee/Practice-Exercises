ecbe02a8067f1fccf217940db15902576c0901fe
Young Lee <ylee8575@gmail.com>
2019-10-28 18:03:09 -0700

    10/28/19 codewars

--- codewars.js
+++ codewars.js
@@ -1643,6 +1643,22 @@ var fizzify = fizzbuzz;
 
 // 8 kyu problems //
 
+/* Twice as Old 
+
+Your function takes two arguments:
+
+current father's age (years)
+current age of his son (years)
+Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). */
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(dadYearsOld - 2 * sonYearsOld);
+}
+
+function twiceAsOld(a, b) {
+  return a>2*b ? a-2*b : 2*b-a;
+}
+
 /* Is the string uppercase?
 
 Is the string uppercase?
