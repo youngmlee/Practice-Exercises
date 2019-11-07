9859457ca49d13145dd5d5dfde57032a7d0444b4
Young Lee <ylee8575@gmail.com>
2019-11-06 19:53:38 -0800

    11/6/19 codewars

--- codewars.js
+++ codewars.js
@@ -1643,6 +1643,22 @@ var fizzify = fizzbuzz;
 
 // 8 kyu problems //
 
+/* Swap Values
+
+I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+
+Can you figure out what's wrong here? */
+
+function swapValues() {
+  return arguments[0].reverse();
+}
+
+function swapValues(args) {
+  var temp = args[0];
+  args[0] = args[1];
+  args[1] = temp;
+}
+
 /* Find the Integral
 
 Create a function that finds the integral of the expression passed.
