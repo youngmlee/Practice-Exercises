f886a2da57d5fbeb182d766fb558da11ae21cf70
Young Lee <ylee8575@gmail.com>
2019-11-18 22:59:16 -0800

    11/18/19 codewars

--- codewars.js
+++ codewars.js
@@ -1674,6 +1674,28 @@ var fizzify = fizzbuzz;
 
 // 8 kyu problems //
 
+/* Draw stairs
+given a number n, draw stairs with 'I' n tall and n wide, with the tallest in the top left. Example (with - to represent spaces; DO NOT USE THEM IN THE SOLUTIONS! USE SPACES IN SOLUTION! the "-"s are for clarity.): draw_stairs(3) == '''I\n_I\n__I'''
+
+For example, a 7-step stairs should be drawn like this:
+
+const sevenStepStairs = drawStairs(7);
+I
+ I
+  I
+   I
+    I
+     I
+      I */
+
+function drawStairs(n) {
+
+let step = "I"
+for(let i=1; i<n;  i++) {
+ step +="\n" + " ".repeat(i) + 'I' ;
+}return step 
+}
+
 /* Sort and Star
 
 You will be given an vector of string(s). You must sort it alphabetically (case-sensitive!!) and then return the first value.
