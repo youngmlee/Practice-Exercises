6180ceb0724560ab8749d73d1edc39defce32799
Young Lee <ylee8575@gmail.com>
2019-11-25 22:50:32 -0800

    11/25/19 codewars

--- codewars.js
+++ codewars.js
@@ -1674,6 +1674,28 @@ var fizzify = fizzbuzz;
 
 // 8 kyu problems //
 
+/* Generate range of integers
+
+Implement a function named generateRange(min, max, step), which takes three arguments and generates a range of integers from min to max, with the step. The first integer is the minimum value, the second is the maximum of the range and the third is the step. (min < max)
+
+Task
+Implement a function named
+
+generateRange(2, 10, 2) // should return array of [2,4,6,8,10]
+generateRange(1, 10, 3) // should return array of [1,4,7,10]
+Note
+min < max
+step > 0
+the range does not HAVE to include max (depending on the step) */
+
+function generateRange(min, max, step){
+  let arr = [];
+  for (let i=min; i<=max; i += step) {
+    arr.push(i);
+  }
+  return arr;
+}
+
 /* L1: Set alarm
 
 Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
