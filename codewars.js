3814861726bdb77eb6e7bdacf26bc6e1d21d813c
Young Lee <ylee8575@gmail.com>
2019-12-09 23:01:25 -0800

    12/9/19 codewars

--- codewars.js
+++ codewars.js
@@ -1771,6 +1771,20 @@ var fizzify = fizzbuzz;
 
 // 8 kyu problems //
 
+/* The 'if' function
+
+Who likes keywords? Nobody likes keywords, so why use them?
+
+You know what keyword I use too much? if! We should make a function called _if, with its arguments as a logical test and two functions/lambdas where the first function is executed if the boolean is true, and the second if it's false, like an if/else statement, so that we don't have to mess around with those nasty keywords! Even so, It should support truthy/falsy types just like the keyword.
+
+Example:
+_if(true, function(){console.log("True")}, function(){console.log("false")})
+// Logs 'True' to the console. */
+
+function _if(bool, func1, func2) {
+  return bool ? func1() : func2();
+}
+
 /* The Wide-Mouthed Frog
 
 The wide mouth frog is particularly interested in the eating habits of other creatures.
