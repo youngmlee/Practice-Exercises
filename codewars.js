84edaffa5666fb5b167edca0a081ac57a66137a7
Young Lee <ylee8575@gmail.com>
2019-12-17 22:21:13 -0800

    12/17/19 codewars

--- codewars.js
+++ codewars.js
@@ -184,6 +184,31 @@ function reverseWords(str){
 
 // 7 kyu problems //
 
+/* Summing a number's digits
+
+Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits. For example:
+
+  sumDigits(10);  // Returns 1
+  sumDigits(99);  // Returns 18
+  sumDigits(-32); // Returns 5
+Let's assume that all numbers in the input will be integer values. */
+
+function sumDigits(number) {
+    var i = 0;
+    var sum = 0;
+    number = Math.abs(number)
+    while(number != 0)
+    {
+        sum += number % 10
+        number = Math.floor(number/10)
+    }
+    return sum
+}
+
+function sumDigits(number) {
+  return Math.abs(number).toString().split('').reduce(function(a,b){return +a + +b}, 0);
+}
+
 /* Borrower Speak
 
 The borrowers are tiny tiny fictional people. As tiny tiny people they have to be sure they aren't spotted, or more importantly, stepped on.
