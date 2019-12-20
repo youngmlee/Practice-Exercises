b4611e381ff0b6cca5132307da220ad1c14bbb55
Young Lee <ylee8575@gmail.com>
2019-12-19 22:51:25 -0800

    12/19/19 leetcode

--- leetcode.js
+++ leetcode.js
@@ -2,6 +2,50 @@
 
 // Easy Difficulty //
 
+/* Happy Number
+
+Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Example: 
+
+Input: 19
+Output: true
+Explanation: 
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1 */
+
+var isHappy = function(n) {
+    
+    let dp = [];
+    
+    return checkHappy(n, dp)
+    
+};
+
+let checkHappy = (n, dp) => {
+     if (n === 1)
+        return true;
+    if (dp.includes(n))
+        return false;
+    else {
+        dp.push(n);
+        
+        let sum = 0;
+        while (n) {
+            sum += (n % 10) ** 2;
+            n = Math.floor(n / 10);
+        }
+        
+        n = sum;
+    }
+    
+    return checkHappy(n, dp)
+}
+
 /* Rising Temperature
 
 SQL Schema
