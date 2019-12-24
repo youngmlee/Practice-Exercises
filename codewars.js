b799ff99ff58e4d01ec519c685b13a6d71c1b27d
Young Lee <ylee8575@gmail.com>
2019-12-24 13:58:12 -0800

    12/24/19 codewars

--- codewars.js
+++ codewars.js
@@ -1837,6 +1837,14 @@ var fizzify = fizzbuzz;
 
 // 8 kyu problems //
 
+/* Easy Logs
+
+Add two logs with base X, with the value of A and B. Example log A + log B where the base is X. */
+
+function logs(x , a, b){
+return (Math.log(a*b) / Math.log(x));
+}
+
 /* Expressions Matter
 
 Task
