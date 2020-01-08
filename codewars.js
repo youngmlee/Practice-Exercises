52baa9f5f678f2a7dd2209e146a5ac966597dd01
Young Lee <ylee8575@gmail.com>
2020-01-07 17:09:04 -0800

    1/7/20 codewars

--- codewars.js
+++ codewars.js
@@ -184,6 +184,29 @@ function reverseWords(str){
 
 // 7 kyu problems //
 
+/* London CityHacker
+
+You are given an array of a journey in London, UK. The array will contain bus numbers and TFL tube names as strings e.g.
+
+['Northern', 'Central', 243, 1, 'Victoria']
+Journeys will always only contain a combination of tube names and bus numbers. Each tube journey costs £2.40 and each bus journey costs £1.50. If there are 2 or more adjacent bus journeys, the bus fare is capped for sets of two adjacent buses and calculated as one bus fare for each set.
+
+Your task is to calculate the total cost of the journey and return the cost rounded to 2 decimal places in the format (where x is a number): £x.xx */
+
+function londonCityHacker(journey) {
+  let sum = 0;
+  
+  for (let i = 0; i < journey.length; i++) {
+    if (typeof journey[i] === "string") sum += 2.40;
+    else {
+      sum += 1.50;
+      if (typeof journey[i + 1] === "number") i++;
+    }  
+  }
+  
+  return `£${sum.toFixed(2)}`;
+}
+
 /* Summing a number's digits
 
 Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits. For example:
